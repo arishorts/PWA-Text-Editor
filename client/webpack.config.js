31ccd7835c26d10cf778e53ae560cc1fcb23f843
arishorts <arieljschwartz@gmail.com>
2023-05-05 21:31:41 -0700

    Bugfixxed error where the dist folder wouldnt be created. App still doesnt display header

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,16 +2,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
-
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: "development",
     entry: {
       main: "./src/js/index.js",
       install: "./src/js/install.js",
+      editor: "./src/js/editor.js",
+      header: "./src/js/header.js",
     },
     output: {
       filename: "[name].bundle.js",
@@ -21,13 +21,14 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles.
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "Contact Cards",
+        title: "J.A.T.E",
       }),
 
       // Injects our custom service worker
       new InjectManifest({
         swSrc: "./src-sw.js",
-        swDest: "src-sw.js",
+        swDest: "service-worker.js",
+        exclude: [/\.(?:png|jpg|jpeg|svg)$/],
       }),
 
       // Creates a manifest.json file.
@@ -35,9 +36,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Contact Cards",
-        short_name: "Contact",
-        description: "Never forget your contacts!",
+        name: "J.A.T.E",
+        short_name: "JATE",
+        description: "Have fun editting text!",
         background_color: "#225ca3",
         theme_color: "#225ca3",
         start_url: "./",
